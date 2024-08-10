21b9e63be002caf2230a8a9518380523116cc15c
siroj <qwrqwqewrq@mail.ru>
2024-08-10 17:48:40 +0500

    final commit

--- app.js
+++ app.js
@@ -21,3 +21,58 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "flex";
 }
 
+"use strict"
+//==========================================
+const TELEGRAM_BOT_TOKEN = '6440356750:AAFaO2zcMnJaLnZvQ5fdLtHqmcjz1vrbOZE';
+const TELEGRAM_CHAT_ID = '@testbuttonsvv';
+const API = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`
+
+
+async function sendEmailTelegram(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const formBtn = document.querySelector('.form__submit-button button')
+    const formSendResult = document.querySelector('.form__send-result')
+    formSendResult.textContent = '';
+
+
+    const { name,  phone} = Object.fromEntries(new FormData(form).entries());
+    
+    const text = `Заявка от ${name}!\nТелефон: ${phone}`;
+
+
+    try {
+        formBtn.textContent = 'Loading...';
+
+        const response = await fetch(API, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                chat_id: TELEGRAM_CHAT_ID,
+                text,
+            })
+        })
+        
+        if (response.ok) {
+            formSendResult.textContent = 'Спасибо за ваше сообщение! Мы свяжемся с вами в ближайшее время.';
+            form.reset()
+        } else {
+            throw new Error(response.statusText);
+        }
+
+    } catch (error) {
+        console.error(error);
+        formSendResult.textContent = 'Анкета не отправлена! Попробуйте позже.';
+        formSendResult.style.color = 'red';
+
+    } finally {
+        formBtn.textContent = 'Отправить';
+    }
+}
+
+
+
+// ----------Новости -------------- 
